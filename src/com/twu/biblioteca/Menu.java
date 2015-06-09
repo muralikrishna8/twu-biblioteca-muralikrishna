4f52d895ec510527b06c2e5abacacadd4f6d346a
muralikrishna8 <vmurali1444@gmail.com>
2015-06-09 23:18:49 +0530

    added spec for selecting a menu item and performing corresponding action

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -3,14 +3,23 @@ package com.twu.biblioteca;
 import java.util.HashMap;
 
 public class Menu {
-    HashMap<Integer, MenuListener> menuListeners;
+    HashMap<String, MenuListener> menuListeners;
     BibliotecaOutput bibliotecaOutput;
+    BibliotecaInput bibliotecaInput;
 
-    public Menu(BibliotecaOutput bibliotecaOutput){
+    public Menu(HashMap<String, MenuListener> menuListeners,
+                BibliotecaOutput bibliotecaOutput, BibliotecaInput bibliotecaInput){
+        this.menuListeners = menuListeners;
         this.bibliotecaOutput = bibliotecaOutput;
+        this.bibliotecaInput = bibliotecaInput;
     }
 
     public void displayMenu() {
         bibliotecaOutput.print("1. List All Books");
+    }
+
+    public void selectFromMenu() {
+        String option = bibliotecaInput.read();
+        menuListeners.get(option).performAction();
     }
 }
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -6,20 +6,51 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MenuTest {
 
     @Mock
+    Library library;
+    @Mock
     BibliotecaOutput bibliotecaOutput;
+    @Mock
+    BibliotecaInput bibliotecaInput;
+    @Mock
+    DisplayBooksList displayBooksList;
+
+    private HashMap<Integer, MenuListener> menuItemListeners;
 
     @Test
     public void specToCheckWhetherPrintingTheMenuListIsCalled() {
-        Menu menu = new Menu(bibliotecaOutput);
+        Menu menu = getMenu();
 
         menu.displayMenu();
 
         Mockito.verify(bibliotecaOutput).print("1. List All Books");
+    }
+
+    @Test
+    public void specForSelectingAMenuItemAndPerformingCorrespondingAction() {
+        Menu menu = getMenu();
+        when(bibliotecaInput.read()).thenReturn("1");
+
+        menu.selectFromMenu();
+
+        Mockito.verify(bibliotecaInput).read();
+        Mockito.verify(displayBooksList).performAction();
+
+    }
+
+    private Menu getMenu() {
+        HashMap<String, MenuListener> menuItemListeners = new HashMap<>();
+
+        menuItemListeners.put("1", displayBooksList);
+
+        return new Menu(menuItemListeners, bibliotecaOutput, bibliotecaInput);
     }
 }
