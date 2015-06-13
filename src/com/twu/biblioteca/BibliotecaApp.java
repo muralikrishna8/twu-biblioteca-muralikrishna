74317a1928a558d369d1e3ddd00cf857c3a45ee7
muralikrishna8 <vmurali1444@gmail.com>
2015-06-14 00:37:23 +0530

    Refactor - Moved main method to new BibliotecatInit class

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,10 +1,5 @@
 package com.twu.biblioteca;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Scanner;
-
 public class BibliotecaApp {
     private BibliotecaIO bibliotecaIO;
     private Menu menu;
@@ -24,27 +19,5 @@ public class BibliotecaApp {
         do {
             menu.displayMenu();
         } while (menu.selectFromMenu());
-    }
-
-    public static void main(String[] args) {
-        HashMap<String, MenuListener> menuItemListeners = new HashMap<>();
-        BibliotecaIO bibliotecaIO = new BibliotecaIO(new Scanner(System.in));
-
-        LinkedHashMap<Book, Boolean> books = new LinkedHashMap<>();
-        books.put(new Book("Book1", "Author1", 2009), true);
-        books.put(new Book("Book2", "Author2", 2011), true);
-        books.put(new Book("Book3", "Author3", 2012), true);
-        Library library = new Library(books);
-
-        DisplayBooksList displayBooksList = new DisplayBooksList(bibliotecaIO, library);
-        menuItemListeners.put("1", displayBooksList);
-
-        ArrayList<String> menuList = new ArrayList<>();
-        menuList.add("1. List Books");
-        menuList.add("2. Quit");
-
-        Menu menu = new Menu(menuItemListeners, bibliotecaIO, menuList);
-
-        new BibliotecaApp(bibliotecaIO, menu).init();
     }
 }
