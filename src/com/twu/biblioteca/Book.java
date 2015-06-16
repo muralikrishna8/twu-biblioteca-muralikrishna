5af25a363afb444c8c2f94539f9aaa07a5fbca34
muralikrishna8 <vmurali1444@gmail.com>
2015-06-16 23:45:02 +0530

    Removed overriden equality method from the Book class

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -19,19 +19,4 @@ public class Book implements LibraryItem{
     public String toString() {
         return String.format("%-50s|    %-40s|     %-7d", title, author, yearOfPublication);
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Book)) return false;
-
-        Book book = (Book) o;
-
-        return title.equals(book.title);
-    }
-
-    @Override
-    public int hashCode() {
-        return title.hashCode();
-    }
 }
